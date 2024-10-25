0116ba7e97d896fb885782adcd73aa912486486d
Artem <atremat@gmail.com>
2024-10-25 23:42:13 +0300

    book modal mobile styles add

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Icon from '../Icon/Icon';
 import styles from './MobileMenu.module.css';
 import Modal from 'react-modal';
@@ -7,25 +7,21 @@ import clsx from 'clsx';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import { AuthNav } from '../AuthNav/AuthNav';
+import MobileNav from '../MobileNav/MobileNav';
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
 
-  const buildLinkClass = ({ isActive }) => {
-    return clsx(styles.link, isActive && styles.active);
-  };
+  // useEffect(() => {
+  //   document.body.classList.add(styles.modalOpen);
 
-  useEffect(() => {
-    document.body.classList.add(styles.modalOpen);
-
-    return () => {
-      document.body.classList.remove(styles.modalOpen);
-    };
-  }, []);
+  //   return () => {
+  //     document.body.classList.remove(styles.modalOpen);
+  //   };
+  // }, []);
 
   return (
     <div className={styles.mobileWrapper}>
@@ -44,47 +40,48 @@ const MobileMenu = () => {
       </div>
 
       {isOpen && (
-        <Modal
-          isOpen={isOpen}
-          onRequestClose={closeModal}
-          className={styles.modal}
-          overlayClassName={styles.overlay}
-          contentLabel="Mobile Menu"
-        >
-          <button className={styles.closeBtn} onClick={closeModal}>
-            <Icon id="close" width="32" height="32" />
-          </button>
+        <MobileNav isOpen={isOpen} closeModal={closeModal} />
+        // <Modal
+        //   isOpen={isOpen}
+        //   onRequestClose={closeModal}
+        //   className={styles.modal}
+        //   overlayClassName={styles.overlay}
+        //   contentLabel="Mobile Menu"
+        // >
+        //   <button className={styles.closeBtn} onClick={closeModal}>
+        //     <Icon id="close" width="32" height="32" />
+        //   </button>
 
-          <nav className={styles.burgerNav}>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <NavLink to="/" className={buildLinkClass} onClick={closeModal}>
-                  Home
-                </NavLink>
-              </li>
-              <li className={styles.item}>
-                <NavLink
-                  to="/teachers"
-                  className={buildLinkClass}
-                  onClick={closeModal}
-                >
-                  Teachers
-                </NavLink>
-              </li>
-              {isLoggedIn && (
-                <li className={styles.item}>
-                  <NavLink
-                    to="/favorites"
-                    className={buildLinkClass}
-                    onClick={closeModal}
-                  >
-                    Favorites
-                  </NavLink>
-                </li>
-              )}
-            </ul>
-          </nav>
-        </Modal>
+        //   <nav className={styles.burgerNav}>
+        //     <ul className={styles.list}>
+        //       <li className={styles.item}>
+        //         <NavLink to="/" className={buildLinkClass} onClick={closeModal}>
+        //           Home
+        //         </NavLink>
+        //       </li>
+        //       <li className={styles.item}>
+        //         <NavLink
+        //           to="/teachers"
+        //           className={buildLinkClass}
+        //           onClick={closeModal}
+        //         >
+        //           Teachers
+        //         </NavLink>
+        //       </li>
+        //       {isLoggedIn && (
+        //         <li className={styles.item}>
+        //           <NavLink
+        //             to="/favorites"
+        //             className={buildLinkClass}
+        //             onClick={closeModal}
+        //           >
+        //             Favorites
+        //           </NavLink>
+        //         </li>
+        //       )}
+        //     </ul>
+        //   </nav>
+        // </Modal>
       )}
     </div>
   );
---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,72 @@
+import { useEffect } from 'react';
+import styles from './MobileNav.module.css';
+import Modal from 'react-modal';
+import clsx from 'clsx';
+import Icon from '../Icon/Icon';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { NavLink } from 'react-router-dom';
+
+const MobileNav = ({ isOpen, closeModal }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    document.body.classList.add(styles.modalOpen);
+
+    return () => {
+      document.body.classList.remove(styles.modalOpen);
+    };
+  }, []);
+
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(styles.link, isActive && styles.active);
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+        contentLabel="Mobile Menu"
+      >
+        <button className={styles.closeBtn} onClick={closeModal}>
+          <Icon id="close" width="32" height="32" />
+        </button>
+
+        <nav className={styles.burgerNav}>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <NavLink to="/" className={buildLinkClass} onClick={closeModal}>
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.item}>
+              <NavLink
+                to="/teachers"
+                className={buildLinkClass}
+                onClick={closeModal}
+              >
+                Teachers
+              </NavLink>
+            </li>
+            {isLoggedIn && (
+              <li className={styles.item}>
+                <NavLink
+                  to="/favorites"
+                  className={buildLinkClass}
+                  onClick={closeModal}
+                >
+                  Favorites
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Modal>
+    </div>
+  );
+};
+
+export default MobileNav;
